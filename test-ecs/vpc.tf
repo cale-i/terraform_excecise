8f3dd105c571164c5b895731069a1f25131e95aa
cale-i <calei078029@gmail.com>
2020-10-30 23:59:50 +0900

    add tags

--- test-ecs/vpc.tf
+++ test-ecs/vpc.tf
@@ -8,6 +8,7 @@ resource "aws_vpc" "main" {
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "${var.prefix}"
   }
@@ -26,6 +27,10 @@ resource "aws_subnet" "public_1a" {
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-public_1a"
+  }
 }
 
 resource "aws_subnet" "public_1c" {
@@ -33,6 +38,10 @@ resource "aws_subnet" "public_1c" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-public_1c"
+  }
 }
 
 #####################
@@ -41,6 +50,10 @@ resource "aws_subnet" "public_1c" {
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
 }
 
 #####################
@@ -82,6 +95,10 @@ resource "aws_subnet" "dmz_1a" {
   cidr_block              = "10.0.32.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-dmz_1a"
+  }
 }
 
 resource "aws_subnet" "dmz_1c" {
@@ -89,6 +106,10 @@ resource "aws_subnet" "dmz_1c" {
   cidr_block              = "10.0.33.0/24"
   availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-dmz_1c"
+  }
 }
 
 resource "aws_route_table" "dmz_1a" {
@@ -129,6 +150,10 @@ resource "aws_subnet" "private_1a" {
   cidr_block              = "10.0.64.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.prefix}-private_1a"
+  }
 }
 
 resource "aws_subnet" "private_1c" {
@@ -136,56 +161,60 @@ resource "aws_subnet" "private_1c" {
   cidr_block              = "10.0.65.0/24"
   availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.prefix}-private_1c"
+  }
 }
 
-resource "aws_eip" "nat_gateway_1a" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.main]
-}
+# resource "aws_eip" "nat_gateway_1a" {
+#   vpc        = true
+#   depends_on = [aws_internet_gateway.main]
+# }
 
-resource "aws_eip" "nat_gateway_1c" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.main]
-}
+# resource "aws_eip" "nat_gateway_1c" {
+#   vpc        = true
+#   depends_on = [aws_internet_gateway.main]
+# }
 
-resource "aws_nat_gateway" "nat_gateway_1a" {
-  allocation_id = aws_eip.nat_gateway_1a.id
-  subnet_id     = aws_subnet.public_1a.id
-  depends_on    = [aws_internet_gateway.main]
-}
+# resource "aws_nat_gateway" "nat_gateway_1a" {
+#   allocation_id = aws_eip.nat_gateway_1a.id
+#   subnet_id     = aws_subnet.public_1a.id
+#   depends_on    = [aws_internet_gateway.main]
+# }
 
-resource "aws_nat_gateway" "nat_gateway_1c" {
-  allocation_id = aws_eip.nat_gateway_1c.id
-  subnet_id     = aws_subnet.public_1c.id
-  depends_on    = [aws_internet_gateway.main]
-}
+# resource "aws_nat_gateway" "nat_gateway_1c" {
+#   allocation_id = aws_eip.nat_gateway_1c.id
+#   subnet_id     = aws_subnet.public_1c.id
+#   depends_on    = [aws_internet_gateway.main]
+# }
 
-resource "aws_route_table" "private_1a" {
-  vpc_id = aws_vpc.main.id
-}
+# resource "aws_route_table" "private_1a" {
+#   vpc_id = aws_vpc.main.id
+# }
 
-resource "aws_route_table" "private_1c" {
-  vpc_id = aws_vpc.main.id
-}
+# resource "aws_route_table" "private_1c" {
+#   vpc_id = aws_vpc.main.id
+# }
 
-resource "aws_route" "private_1a" {
-  route_table_id         = aws_route_table.private_1a.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_1a.id
-  destination_cidr_block = "0.0.0.0/0"
-}
+# resource "aws_route" "private_1a" {
+#   route_table_id         = aws_route_table.private_1a.id
+#   nat_gateway_id         = aws_nat_gateway.nat_gateway_1a.id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
 
-resource "aws_route" "private_1c" {
-  route_table_id         = aws_route_table.private_1c.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_1c.id
-  destination_cidr_block = "0.0.0.0/0"
-}
+# resource "aws_route" "private_1c" {
+#   route_table_id         = aws_route_table.private_1c.id
+#   nat_gateway_id         = aws_nat_gateway.nat_gateway_1c.id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
 
-resource "aws_route_table_association" "private_1a" {
-  subnet_id      = aws_subnet.private_1a.id
-  route_table_id = aws_route_table.private_1a.id
-}
+# resource "aws_route_table_association" "private_1a" {
+#   subnet_id      = aws_subnet.private_1a.id
+#   route_table_id = aws_route_table.private_1a.id
+# }
 
-resource "aws_route_table_association" "private_1c" {
-  subnet_id      = aws_subnet.private_1c.id
-  route_table_id = aws_route_table.private_1c.id
-}
+# resource "aws_route_table_association" "private_1c" {
+#   subnet_id      = aws_subnet.private_1c.id
+#   route_table_id = aws_route_table.private_1c.id
+# }
