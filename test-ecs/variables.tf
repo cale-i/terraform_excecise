8e9d653fdd9650c786045af2b0f5d542d51f77cf
cale-i <calei078029@gmail.com>
2020-11-05 17:53:25 +0900

    add variables

--- test-ecs/variables.tf
+++ test-ecs/variables.tf
@@ -1,9 +1,24 @@
 variable "region" {
   default = "ap-northeast-1"
 }
+# variable "region" {
+#   default = data.aws_region.current.name
+# }
 
 variable "prefix" {
   default = "ecs"
+}
+
+variable "docker_dir" {
+  default = "."
+}
+
+
+variable "container_name" {
+  default = "webserver"
+}
+variable "docker_image_tag" {
+  default = "latest"
 }
 
 variable "domain" {
@@ -15,4 +30,16 @@ variable "bucket_account_id" {
   default = {
     "ap-northeast-1" = "582318560864"
   }
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+data "aws_region" "current" {}
+
+output "region_name" {
+  value = data.aws_region.current.name
 }
