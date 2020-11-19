27423a4c26b715395e104447b0617080bb7cbaa6
cale-i <calei078029@gmail.com>
2020-11-19 15:32:09 +0900

    add debug settings

--- nginx-gunicorn-django/backend/config/urls.py
+++ nginx-gunicorn-django/backend/config/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls import re_path
 from django.views.generic import TemplateView
 from django.views.generic import RedirectView
 
+
+from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apiv1.urls')),
@@ -12,3 +16,10 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
 
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
